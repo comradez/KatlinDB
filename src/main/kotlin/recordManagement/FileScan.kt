c2cbb2993b3844d6def4a4e4739cd69b8b208921
ChrisZhang <zcyjim@outlook.com>
2022-01-07 14:50:02 +0800

    修正了 FileScan，允许在不比较时传入 null value，同时设置默认扫描范围为整个Page

--- src/main/kotlin/recordManagement/FileScan.kt
+++ src/main/kotlin/recordManagement/FileScan.kt
@@ -1,6 +1,7 @@
 package recordManagement
 
 import com.github.michaelbull.result.get
+import utils.PAGE_SIZE
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
@@ -8,37 +9,43 @@ class FileScan {
     fun openScan(
         fileHandler: FileHandler,
         attrType: AttributeType,
-        attrLength: Int,
-        attrOffset: Int,
+        attrLength: Int?,
+        attrOffset: Int?,
+        // 如果两个都给了非 null 值那么选取 [attrOffset, attrOffset + attrLength)
+        // 只要有一个 null 那么选取的是整个 Record 的范围，即 [0, PAGE_SIZE)
         compareOp: CompareOp,
-        value: Any // 可能是 String, Int 或者 Float
+        value: Any? // 可能是 String, Int 或者 Float
     ) = sequence {
             val availableRIDs = fileHandler.getItemRIDs().iterator()
             while (availableRIDs.hasNext()) {
                 val record = checkNotNull(fileHandler.getRecord(availableRIDs.next()).get())
-                val requiredAttrBytes = record.data.sliceArray(attrOffset until attrOffset + attrLength)
+                val requiredAttrBytes = record.data.sliceArray(if (attrOffset != null && attrLength != null) {
+                    attrOffset until attrOffset + attrLength
+                } else {
+                    0 until PAGE_SIZE
+                })
                 when (attrType) {
                     AttributeType.INT -> {
                         val requiredAttr = ByteBuffer.wrap(requiredAttrBytes).order(ByteOrder.LITTLE_ENDIAN).int
-                        if (comp(requiredAttr, value as Int, compareOp)) { // 这里改成比较函数
+                        if (comp(requiredAttr, value as Int?, compareOp)) { // 这里改成比较函数
                             yield(record)
                         }
                     }
                     AttributeType.FLOAT -> {
                         val requiredAttr = ByteBuffer.wrap(requiredAttrBytes).order(ByteOrder.LITTLE_ENDIAN).float
-                        if (comp(requiredAttr, value as Float, compareOp)) {
+                        if (comp(requiredAttr, value as Float?, compareOp)) {
                             yield(record)
                         }
                     }
                     AttributeType.STRING -> {
                         val requiredAttr = requiredAttrBytes.decodeToString()
-                        if (strComp(requiredAttr, value as String, compareOp)) {
+                        if (strComp(requiredAttr, value as String?, compareOp)) {
                             yield(record)
                         }
                     }
                     AttributeType.LONG -> {
                         val requiredAttr = ByteBuffer.wrap(requiredAttrBytes).order(ByteOrder.LITTLE_ENDIAN).long
-                        if (comp(requiredAttr, value as Long, compareOp)) {
+                        if (comp(requiredAttr, value as Long?, compareOp)) {
                             yield(record)
                         }
                     }
@@ -46,20 +53,28 @@ class FileScan {
             }
         }
 
-    private fun<T: Comparable<T>> comp(first: T, second: T, op: CompareOp): Boolean = when (op) {
-        CompareOp.GT_OP -> first > second
-        CompareOp.LT_OP -> first < second
-        CompareOp.GE_OP -> first >= second
-        CompareOp.LE_OP -> first <= second
-        CompareOp.EQ_OP -> first == second
-        CompareOp.NE_OP -> first != second
-        CompareOp.NO_OP -> true
+    private fun<T: Comparable<T>> comp(first: T, second: T?, op: CompareOp): Boolean {
+        return if (second != null) {
+            when (op) {
+                CompareOp.GT_OP -> first > second
+                CompareOp.LT_OP -> first < second
+                CompareOp.GE_OP -> first >= second
+                CompareOp.LE_OP -> first <= second
+                CompareOp.EQ_OP -> first == second
+                CompareOp.NE_OP -> first != second
+                CompareOp.NO_OP -> true
+            }
+        } else { true }
     }
 
 
-    private fun strComp(first: String, second: String, op: CompareOp): Boolean = when (op) {
-        CompareOp.EQ_OP -> first == second
-        CompareOp.NE_OP -> first != second
-        else -> true // 字符串只能比较相等或不等，输入其他 op 视同 NO_OP
+    private fun strComp(first: String, second: String?, op: CompareOp): Boolean {
+        return if (second != null) {
+            when (op) {
+                CompareOp.EQ_OP -> first == second
+                CompareOp.NE_OP -> first != second
+                else -> true // 字符串只能比较相等或不等，输入其他 op 视同 NO_OP
+            }
+        } else { true }
     }
 }
